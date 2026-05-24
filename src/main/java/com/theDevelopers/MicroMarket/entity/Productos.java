dfb1b8e5101fdabf714ba1b778dd51679e092cb2
w-danieldevs <wdsaavedra408@gmail.com>
2026-05-24 16:35:18 -0500

    feat: se sube entity pero sin venta

--- src/main/java/com/theDevelopers/MicroMarket/entity/Productos.java
+++ src/main/java/com/theDevelopers/MicroMarket/entity/Productos.java
@@ -1,5 +1,32 @@
 package com.theDevelopers.MicroMarket.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Productos")
+@Data
+
 public class Productos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_productos;
+
+    @Column(name = "nombre_producto" )
+    private String nombre_producto;  
     
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "precio")
+    private float precio;
+
+    @Column(name = "activo")
+    private boolean activo;
+
+    @Column(name = "codigo_barras")
+    private String codigo_barras;
+
+
 }
--- src/main/java/com/theDevelopers/MicroMarket/entity/Proveedores.java
+++ src/main/java/com/theDevelopers/MicroMarket/entity/Proveedores.java
@@ -1,5 +1,19 @@
 package com.theDevelopers.MicroMarket.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Proveedores")
+@Data
+
 public class Proveedores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_proveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombre_proveedor;
     
 }
